7c91789dc4cd97635ccdba06c4786fc708909a04
zk2138 <zk2138@student.uni-lj.si>
2016-05-18 14:51:01 +0000

    Dodana funkcionalnost za dodajanje pacienta ročno in izdelan izgled strani za dodajanje

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -61,6 +61,56 @@ function generirajPodatke(stPacienta) {
     return ehrId;
 }
 
+function EHRzaPacienta() {
+	sessionId = getSessionId();
+
+	var ime = $("#vpisiIme").val();
+	var priimek = $("#vpisiPriimek").val();
+	var datumRojstva = $("#vpisiDatumR").val();
+
+	if (!ime || !priimek || !datumRojstva || ime.trim().length == 0 ||
+      priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
+		$("#sporociloP").html("<span class='obvestilo label " +
+      "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#sporociloP").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#ehrIdP").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#sporociloP").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		    }
+		});
+	}
+}
+
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
 
